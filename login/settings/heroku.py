f1cfb5fa5af6bce2ba21300686f9d0d6f759c442
robertomacedo <macedorss20@gmail.com>
2021-07-02 16:10:56 -0300

    resoluçao de conflito

--- login/settings/heroku.py
+++ login/settings/heroku.py
@@ -7,11 +7,7 @@ env = environ.Env()
 
 DEBUG = env.bool('DEBUG', False)
 
-<<<<<<< HEAD
-SECRET_KEY = env('SECRETY_KEY')
-=======
 SECRET_KEY = env('SECRET_KEY')
->>>>>>> d3e761c1907f82a852afc57ee51713c56c5327a9
 
 ALLOWED_HOSTS = list('ALLOWED_HOSTS')
 
