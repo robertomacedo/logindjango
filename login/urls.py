d9df20f8fb7d0c2d0f94e9a32c8fc490a356703d
robertomacedo <macedorss20@gmail.com>
2021-07-02 20:38:16 -0300

    Teste root

--- login/urls.py
+++ login/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
 
 
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
