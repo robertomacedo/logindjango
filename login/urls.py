a6e0a22b2bb769dfc00d38d9160b79bb8ac7a6f2
robertomacedo <macedorss20@gmail.com>
2021-07-02 21:04:42 -0300

    Atualizado media

--- login/urls.py
+++ login/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
-from . import settings
+# from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+# from . import settings
 
 urlpatterns = [
     # django admin
