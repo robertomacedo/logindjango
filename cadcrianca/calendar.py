7f6e6ce99ccb8839ef7a5d01ececd613f1bc66e2
robertomacedo <macedorss20@gmail.com>
2021-07-09 19:45:05 -0300

    excluido modulo calendar

--- cadcrianca/calendar.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class CalendarWidget(forms.TextInput):
-    class Media:
-        css = {
-            'all': ('pretty.css',)
-        }
-        js = ('animations.js', 'actions.js')
